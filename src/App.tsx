894731db98c89ba7b68a8359c02d76265ea14faa
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-10-27 16:40:49 +0900

    init : 타이포그래피 & 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { MobileLayout } from './components/layout/MobileLayout';
+
 function App() {
   return (
-    <>
+    <MobileLayout>
       <h1 className="text-3xl font-bold underline text-blue-100">SUBOM</h1>
-    </>
+    </MobileLayout>
   );
 }
 
---
+++ src/layout/MobileLayout.tsx
@@ -0,0 +1,7 @@
+interface MobileLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MobileLayout: React.FC<MobileLayoutProps> = ({ children }) => {
+  return <div className={'mobile-layout '}>{children}</div>;
+};
