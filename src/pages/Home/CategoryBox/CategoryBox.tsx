9ab814663d603ad5b21eaac68a9266c2487f7210
hyeon-aa <ahhyun1120@naver.com>
2025-10-31 22:12:39 +0900

    fix: 모바일 터치 시 그림자 추가

--- src/pages/Home/CategoryBox/CategoryBox.tsx
+++ src/pages/Home/CategoryBox/CategoryBox.tsx
@@ -16,7 +16,7 @@ export const CategoryBox = ({
       onClick={handleClickBox}
       className={`rounded-xl p-4 relative bg-[var(--color-white)] ${size === 'large' ? 'flex-2 min-h-[168px]' : 'flex-1 min-h-[109px]'}
            transition-shadow duration-300
-           hover:shadow-[0_0_30px_0_#D0D2D9]`}
+           hover:shadow-[0_0_30px_0_#D0D2D9] active:shadow-[0_0_30px_0_#D0D2D9]`}
     >
       <div className="B02_B" style={{ color: titleColor }}>
         {title}
