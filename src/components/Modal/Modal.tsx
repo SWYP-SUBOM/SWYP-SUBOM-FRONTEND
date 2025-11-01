c1b0221a5c579c1dd44223e30aaf1ea06d67bf05
hyeon-aa <ahhyun1120@naver.com>
2025-11-01 11:30:49 +0900

    feat: modal ui 구현

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,70 @@
+import type { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+import xButton from '../../assets/Modal/xbutton.svg';
+
+export const Overlay = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="absolute inset-0 bg-[#121212]/50 flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export const Xbutton = () => {
+  return (
+    <button
+      className="cursor-pointer rounded-[10px] absolute top-5 right-5 w-8 h-8 flex items-center justify-center hover:bg-[#E7EBEE] active:bg-[#E7EBEE]"
+      type="button"
+    >
+      <img src={xButton} className="w-8 h-8" alt="xButton" />
+    </button>
+  );
+};
+
+export const Content = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="relative bg-[#F9F9F9] max-w-[328px]  max-h-[288px] w-full py-6 rounded-2xl px-5">
+      {children}
+    </div>
+  );
+};
+
+export const Title = ({ children }: { children: ReactNode }) => {
+  return <div className="items-center justify-center T02_B pt-[56px]">{children}</div>;
+};
+
+export const Description = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="inline-block items-center justify-center text-center max-w-[240px] break-normal whitespace-normal B01_M pt-[10px] pb-[31px] text-gray-800">
+      {children}
+    </div>
+  );
+};
+
+export const Trigger = ({ children }: { children: ReactNode }) => {
+  return (
+    <button
+      className="cursor-pointer rounded-xl 
+    bg-[var(--color-b7)] active:bg-[var(--color-b8)]
+    hover:bg-[var(--color-b8)] w-full h-14 B02_B text-white"
+    >
+      {children}
+    </button>
+  );
+};
+
+export const Modal = ({ children }: { children: ReactNode }) => {
+  return createPortal(
+    <div className="fixed inset-0 flex flex-col items-center justify-center text-center">
+      {children}
+    </div>,
+    document.body,
+  );
+};
+
+Modal.Content = Content;
+Modal.Overlay = Overlay;
+Modal.Xbutton = Xbutton;
+Modal.Title = Title;
+Modal.Description = Description;
+Modal.Trigger = Trigger;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
+import { Modal, Xbutton } from '../../components/Modal/Modal';
+
+export const MovetoWriteModal = () => {
+  return (
+    <Modal>
+      <Modal.Overlay>
+        <Modal.Content>
+          <Xbutton></Xbutton>
+          <Modal.Title>일상</Modal.Title>
+          <Modal.Description>
+            아침형 인간과 저녁형 인간 중, 어느 쪽이 더 효율적이라고 생각하시나요?
+          </Modal.Description>
+          <Modal.Trigger>글 쓰러가기</Modal.Trigger>
+        </Modal.Content>
+      </Modal.Overlay>
+    </Modal>
+  );
+};
+
 const Home = () => {
   return (
     <>
-      <p>home</p>
+      <MovetoWriteModal />
     </>
   );
 };
