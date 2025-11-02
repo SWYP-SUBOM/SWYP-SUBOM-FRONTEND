2897db6f4ca27ef8994af1f95924451c19f2cf06
hyeon-aa <ahhyun1120@naver.com>
2025-11-02 22:56:11 +0900

    feat: categorychip 컴포넌트 추가

--- src/pages/Write/index.tsx
+++ src/pages/Write/index.tsx
@@ -1,14 +1,20 @@
 import { useLocation } from 'react-router-dom';
+import { CategoryChip } from '../../components/common/CategoryChip';
 
 export const Write = () => {
   const location = useLocation();
 
   const categoryName = location.state.categoryName;
   const topicName = location.state.topicName;
+
   return (
     <>
-      <div>{categoryName}</div>
-      <div>{topicName}</div>
+      <div className="px-4 bg-[#F3F5F8]">
+        <div className="pt-[30px] pb-3">
+          <CategoryChip categoryName={categoryName}></CategoryChip>
+          <div className="py-[10px] B01_B">{topicName}</div>
+        </div>
+      </div>
     </>
   );
 };
