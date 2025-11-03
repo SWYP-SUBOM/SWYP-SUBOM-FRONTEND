86b3aa8049e0600b8d046fa5c4f003cd2e9dbb56
hyeon-aa <ahhyun1120@naver.com>
2025-11-03 22:56:10 +0900

    feat: 오늘의 주제 박스

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,7 +1,21 @@
+import { TodayTopicBox } from './_components/TodayTopicBox';
+
 const Feed = () => {
+  const topic = {
+    topicId: 'uuid-topic-1',
+    topicText: '카공에 대해 어떻게 생각하나요?',
+    category: {
+      categoryId: 'uuid-category-1',
+      categoryText: '취미·취향',
+    },
+  };
+
   return (
     <>
-      <p>feed</p>
+      <div className="pt-10">
+        <TodayTopicBox topicText={topic.topicText} categoryText={topic.category.categoryText} />
+      </div>
+      <div className="B02_M text-gray-900 pt-6 px-6">다른 사람들은 어떻게 생각할까요?</div>
     </>
   );
 };
