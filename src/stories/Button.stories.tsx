beb4f7b7e130a2147d37b5c1952bbd604efe4c8c
KimTaeWoo <70637743+kim3360@users.noreply.github.com>
2025-11-06 02:10:16 +0900

    fix : button 컴포넌트 구현 (#10)
    * fix : 공통 버튼 컴포넌트 수정
    
    * fix : 아이콘 props 추가
    
    * fix : 버튼 스토리북 수정

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../components/common/Button';
+import kakao from '../assets/Onboarding/kakao.png';
 
 const meta: Meta<typeof Button> = {
   title: 'SUBOM/Button',
@@ -12,14 +13,13 @@ type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
   args: {
-    label: 'Primary Button',
-    primary: true,
+    label: '피드백 받기',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Secondary Button',
-    primary: false,
+    label: '카카오톡 소셜 로그인',
+    icon: kakao,
   },
 };
