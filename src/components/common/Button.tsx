f5fc69a34e97639d25c0691438e32b661b26572a
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-11-05 22:27:59 +0900

    fix : 아이콘 props 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,13 +1,15 @@
 interface ButtonProps {
   label: string;
   primary?: string;
+  icon?: string;
 }
 
-export const Button = ({ label, primary }: ButtonProps) => {
+export const Button = ({ label, primary, icon }: ButtonProps) => {
   return (
     <button
-      className={`B02_B w-82 h-14 mx-4  rounded-xl bg-b7 text-white active:bg-b8 active:scale-95 hover:bg-b8 transition-colors duration-300 cursor-pointer ${primary}`}
+      className={`B02_B w-82 h-14 mx-4 rounded-xl bg-b7 text-white active:bg-b8 active:scale-95 hover:bg-b8 transition-colors duration-300 cursor-pointer flex items-center justify-center gap-2 ${primary}`}
     >
+      {icon && <img src={icon} alt="icon" className="w-4.5 h-4.5" />}
       {label}
     </button>
   );
