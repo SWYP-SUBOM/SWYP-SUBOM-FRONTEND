49a05ebfaeb66be20c45b0561aae15a0afcd5736
hyeon-aa <ahhyun1120@naver.com>
2025-11-06 19:08:15 +0900

    fix : navbar 배경색 변경

--- src/layout/NavBar/NavBar.tsx
+++ src/layout/NavBar/NavBar.tsx
@@ -8,7 +8,7 @@ export const NavBar = () => {
   return (
     <div
       className="fixed bottom-0 left-1/2 -translate-x-1/2 flex justify-between items-center gap-8 max-w-[360px] w-full
-       py-2.5 bg-[#F9F9F9]/80 backdrop-blur-md rounded-xl shadow-[0px_10px_50px_0px_#D0D2D9] px-[17px]"
+       py-2.5 bg-[#F9F9F9] backdrop-blur-md rounded-xl shadow-[0px_10px_50px_0px_#D0D2D9] px-[17px]"
       style={{ paddingBottom: `calc(10px + env(safe-area-inset-bottom))` }}
     >
       {NAVBAR_ITEMS.map((item) => (
