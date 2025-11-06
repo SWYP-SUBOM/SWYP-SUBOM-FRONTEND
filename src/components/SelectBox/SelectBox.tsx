9801bdea46d7250e31022e912fdd012c929710a7
hyeon-aa <ahhyun1120@naver.com>
2025-11-06 15:53:00 +0900

    feat: select하는 bottomsheet 구현

---
+++ src/components/SelectBox/SelectBox.tsx
@@ -0,0 +1,20 @@
+import down from '../../assets/Feed/down.svg';
+
+type SelectBoxProps = {
+  label: string;
+  onClick: () => void;
+};
+
+export const SelectBox = ({ label, onClick }: SelectBoxProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className="cursor-pointer flex border border-[#D0D2D9] w-fit px-[10px] py-2 rounded-lg gap-[6px]"
+    >
+      <div className="B02_M text-gray-800">{label}</div>
+      <div>
+        <img src={down} className="w-6 h-6" />
+      </div>
+    </div>
+  );
+};
--- src/pages/GatherTopic/_components/GatherTopicContent.tsx
+++ src/pages/GatherTopic/_components/GatherTopicContent.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { SelectBox } from '../../../components/SelectBox/SelectBox';
+import { SelectSortBottomSheet } from './SelectSortBottomSheet';
+
 export const GatherTopicContent = ({ categoryName }: { categoryName: string }) => {
   const questions = [
     {
@@ -13,17 +17,36 @@ export const GatherTopicContent = ({ categoryName }: { categoryName: string }) =
     },
   ];
 
+  const selectSortItems = ['최신순', '오래된순'];
+
+  const [selectedSort, setSelectedSort] = useState('최신순');
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
   return (
-    <div className="flex flex-col gap-[10px] pt-10">
-      {questions.map((question) => (
-        <>
-          <div key={question.topicId}>
-            <div className="B03_M text-gray-900 pb-[14px]">{question.questionText}</div>
-            <div className="C01_SB text-gray-700 text-right mt-1">{question.createdAt}</div>
-            <div className="border-t border-[#E0E4E7] my-4"></div>
-          </div>
-        </>
-      ))}
-    </div>
+    <>
+      <div className="pt-[26px] flex justify-end">
+        <SelectBox label={selectedSort} onClick={() => setIsBottomSheetOpen(true)} />
+      </div>
+      <div className="flex flex-col gap-[10px] pt-[14px]">
+        {questions.map((question) => (
+          <>
+            <div key={question.topicId}>
+              <div className="B03_M text-gray-900 pb-[14px]">{question.questionText}</div>
+              <div className="C01_SB text-gray-700 text-right mt-1">{question.createdAt}</div>
+              <div className="border-t border-[#E0E4E7] my-4"></div>
+            </div>
+          </>
+        ))}
+      </div>
+      {isBottomSheetOpen && (
+        <SelectSortBottomSheet
+          title="정렬 기준"
+          selectItems={selectSortItems}
+          selected={selectedSort}
+          onSelect={setSelectedSort}
+          onClose={() => setIsBottomSheetOpen(false)}
+        />
+      )}
+    </>
   );
 };
