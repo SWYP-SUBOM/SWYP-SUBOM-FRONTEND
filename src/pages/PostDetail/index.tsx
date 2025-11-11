80b28e335d5d37076fb81ae93a91325f387f4ce3
hyeon-aa <ahhyun1120@naver.com>
2025-11-11 19:21:10 +0900

    feat: 리액션 비활성 아이콘 추가 및 reactionbutton 파일 분리

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,21 +1,14 @@
 import { useParams } from 'react-router-dom';
-import freshButton from '../../assets/Feed/fresh_button.png';
-import likeButton from '../../assets/Feed/like_button.png';
-import relateButton from '../../assets/Feed/relate_button.png';
+import { ReactionButtons } from '../../constants/ReactionButtons';
 import { useGetPost } from '../../hooks/Post/useGetPost';
 import { PostDetailBox } from './_components/PostDetailBox';
+import { ReactionButton } from './_components/ReactionButton';
 
 export const PostDetail = () => {
   const params = useParams<{ postId: string }>();
   const postId = Number(params.postId);
 
   const { data: postData } = useGetPost(postId);
-
-  const reactionButtons = [
-    { reactionName: '좋아요', icon: likeButton },
-    { reactionName: '공감돼요', icon: relateButton },
-    { reactionName: '새로워요', icon: freshButton },
-  ];
 
   return (
     <div className="px-4 pt-10">
@@ -30,10 +23,15 @@ export const PostDetail = () => {
         ></PostDetailBox>
       )}
       <div className="justify-end flex gap-[22px] pt-4">
-        {reactionButtons.map((reactionButton) => (
+        {ReactionButtons.map((reactionButton) => (
           <button key={reactionButton.reactionName} className="flex flex-col items-center gap-1">
-            <img src={reactionButton.icon} className="w-15 h-15" />
-            <div className="C01_M text-gray-700">{reactionButton.reactionName}</div>
+            <ReactionButton
+              reactionName={reactionButton.reactionName}
+              icon={reactionButton.icon}
+              officon={reactionButton.officon}
+              isReactioned={false}
+              reactionValue={reactionButton.reactionValue}
+            ></ReactionButton>
           </button>
         ))}
       </div>
