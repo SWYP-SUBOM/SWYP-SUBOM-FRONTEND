95d122b998898aa9b112aba60876f5961b76f47a
hyeon-aa <ahhyun1120@naver.com>
2025-11-15 09:50:43 +0900

    feat: 피드 전체 조회 api 연동

--- src/pages/Feed/_components/FeedContent.tsx
+++ src/pages/Feed/_components/FeedContent.tsx
@@ -1,54 +1,20 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useGetFeed } from '../../../hooks/Feed/useGetFeed';
 import { PostBoxSkeleton } from '../Skeleton/PostBoxSkeleton';
 import { TodayTopicBoxSkeleton } from '../Skeleton/TodayTopicBoxSkeleton';
 import { PostBox } from './PostBox';
 import { TodayTopicBox } from './TodayTopicBox';
 
 const FeedContent = ({ categoryId }: { categoryId: number }) => {
-  /* categoryName에 해당하는 api 연결*/
-  const topic = {
-    topicId: 'uuid-topic-1',
-    topicText: '카공에 대해 어떻게 생각하나요?',
-    category: {
-      categoryId: 'uuid-category-1',
-      categoryText: '취미·취향',
-    },
-  };
+  const {
+    data: feedData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+  } = useGetFeed(categoryId);
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  const items = [
-    {
-      postId: 2,
-      user: {
-        userId: 'uuid-user-A',
-        nickname: '귀여운 코알라',
-      },
-      summary: '가장 효율적인 사람은 자신의 몸이 가장 잘 깨어나는 시간을 아는 사람입니다',
-      status: 'COMPLETED',
-      completedAt: '2025-10-26T10:00:00Z',
-      metrics: {
-        reactionCount: 12,
-        viewCount: 150,
-      },
-    },
-
-    {
-      postId: 3,
-      user: {
-        userId: 'uuid-user-A',
-        nickname: '귀여운 코알라',
-      },
-      summary: '가장 효율적인 사람은 자신의 몸이 가장 잘 깨어나는 시간을 아는 사람입니다',
-      status: 'COMPLETED',
-      completedAt: '2025-10-26T10:00:00Z',
-      metrics: {
-        reactionCount: 12,
-        viewCount: 150,
-      },
-    },
-  ];
+  const allPosts = feedData?.pages.flatMap((page) => page.postList) ?? [];
 
   const navigate = useNavigate();
   const movetoDetail = (postId: number) => {
@@ -68,18 +34,23 @@ const FeedContent = ({ categoryId }: { categoryId: number }) => {
         </>
       ) : (
         <>
-          <TodayTopicBox topicText={topic.topicText} categoryText={topic.category.categoryText} />
+          {feedData && (
+            <TodayTopicBox
+              topicText={feedData.pages[0].topicName}
+              categoryText={feedData.pages[0].categoryName}
+            />
+          )}
           <div className="B02_M text-gray-900 pt-6 px-4 pb-3">다른 사람들은 어떻게 생각할까요?</div>
           <div className="px-4 flex flex-col gap-4">
-            {items.map((item) => (
+            {allPosts.map((post) => (
               <PostBox
-                key={item.postId}
-                onClick={() => movetoDetail(item.postId)}
-                nickname={item.user.nickname}
-                summary={item.summary}
-                heart={item.metrics.reactionCount}
-                completedAt={item.completedAt}
-                view={item.metrics.viewCount}
+                key={post.postId}
+                onClick={() => movetoDetail(post.postId)}
+                nickname={post.nickname}
+                summary={post.summary}
+                heart={post.totalReactions}
+                completedAt={post.updatedAt}
+                view={post.postViews}
               />
             ))}
           </div>
