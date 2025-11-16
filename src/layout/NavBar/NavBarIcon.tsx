a3d997b7aa92071e9a567793de64a02bd2196e6d
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-11-16 17:27:52 +0900

    fix : 타입 명시

--- src/layout/NavBar/NavBarIcon.tsx
+++ src/layout/NavBar/NavBarIcon.tsx
@@ -10,10 +10,15 @@ type NavBarTypes = {
 
 export const NavBarIcon = ({ icon, active, iconActive, menuName, path }: NavBarTypes) => {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(path);
+  };
+
   return (
     <button
       className={`cursor-pointer rounded-[14px] flex flex-col items-center justify-center w-[54px] h-[60px] gap-[6px] ${active ? 'bg-[var(--color-b1)]' : 'bg-[#F9F9F9]'}`}
-      onClick={() => navigate(path)}
+      onClick={handleClick}
     >
       <img className="w-8 h-8" src={active ? iconActive : icon} alt="NavBarIcon" />
       <span className={`${active ? 'text-gray-900' : 'text-[#5A5B66]'} C01_SB`}>{menuName}</span>
