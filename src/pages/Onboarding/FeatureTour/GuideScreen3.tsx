07ad5a9546ab21e1cacbdceb65d77c54bc3fdba6
KimTaeWoo <70637743+kim3360@users.noreply.github.com>
2025-11-22 19:50:42 +0900

    fix : 1차 qa 수정사항 반영 온보딩 (#53)

--- src/pages/Onboarding/FeatureTour/GuideScreen3.tsx
+++ src/pages/Onboarding/FeatureTour/GuideScreen3.tsx
@@ -1,12 +1,13 @@
 import { OnboardingLayout } from '../../../layout/Onboarding/OnboardingLayout';
 import { ProgressIndicator } from '../_components/ProgressIndicator';
 import { useOnboardingNavigation } from '../../../hooks/useOnboardingNavigation';
+import { Button } from '../../../components/common/Button';
 import guide3 from '../../../assets/Onboarding/guide3.png';
 export const GuideScreen3 = () => {
-  const { handleNext } = useOnboardingNavigation();
+  const { handleNext, handleSkip } = useOnboardingNavigation();
   return (
     <>
-      <ProgressIndicator activeIndexes={[2]} total={3} />
+      <ProgressIndicator activeIndexes={[2]} total={4} />
 
       <OnboardingLayout
         title={
@@ -27,11 +28,12 @@ export const GuideScreen3 = () => {
         }}
       />
       <div className="absolute top-[520px] sm:top-[654px] left-0 right-0 flex flex-col justify-center items-center px-4  z-5">
+        <Button label="다음으로" onClick={handleNext} />
         <button
-          onClick={handleNext}
-          className="w-full h-14 bg-b7 rounded-xl text-white B02_B cursor-pointer active:bg-b8 active:scale-95  hover:bg-b8  transition-colors duration-300 "
+          onClick={handleSkip}
+          className="w-20 h-10  text-gray-700 B03_M underline cursor-pointer mt-[2dvh] active:bg-gray-400 active:scale-95  hover:bg-gray-400  rounded-lg transition-colors duration-300  "
         >
-          시작하기
+          건너뛰기
         </button>
       </div>
     </>
