0532453c19e86fef45958c4200247c9ac13682bf
hyeon-aa <ahhyun1120@naver.com>
2025-11-25 23:18:55 +0900

    fix : 오늘의 질문 선택시 이전 질문 조회되는 이슈 수정

--- src/pages/Home/CategoryBox/CategoryBox.tsx
+++ src/pages/Home/CategoryBox/CategoryBox.tsx
@@ -1,4 +1,6 @@
+import { homeService } from '../../../api/services/homeService';
 import { useModal } from '../../../hooks/useModal';
+import { queryClient } from '../../../main';
 import { useTodayPostInfoStore } from '../../../store/useTodayPostInfo';
 import { CompleteDailyQuestionModal } from '../_components/CompleteDailyQuestionModal';
 import { DailyQuestionModal } from '../_components/DailyQuestionModal';
@@ -16,11 +18,18 @@ export const CategoryBox = ({
   const todayPost = useTodayPostInfoStore((state) => state.todayPost);
   const isTodayPublished = todayPost.postStatus === 'PUBLISHED';
 
+  const prefetchQuestion = (categoryId: number) => {
+    queryClient.prefetchQuery({
+      queryKey: ['dailyquestion', categoryId],
+      queryFn: () => homeService.getDailyQuestion(categoryId),
+    });
+  };
+
   const handleModalOpen = (categoryId: number) => {
     if (isTodayPublished) {
       openModal(<CompleteDailyQuestionModal />);
     } else {
-      openModal(<DailyQuestionModal categoryId={categoryId} />);
+      openModal(<DailyQuestionModal categoryId={categoryId} key={categoryId} />);
     }
   };
 
@@ -28,6 +37,7 @@ export const CategoryBox = ({
     <>
       <div
         onClick={() => handleModalOpen(categoryId)}
+        onMouseEnter={() => prefetchQuestion(categoryId)}
         className={`rounded-xl p-4 relative bg-[var(--color-white)] ${size === 'large' ? 'flex-2 min-h-[136px]' : 'flex-1 min-h-[89px]'}
            transition-shadow duration-300 cursor-pointer
            hover:shadow-[0_0_30px_0_#D0D2D9] active:shadow-[0_0_30px_0_#D0D2D9]`}
