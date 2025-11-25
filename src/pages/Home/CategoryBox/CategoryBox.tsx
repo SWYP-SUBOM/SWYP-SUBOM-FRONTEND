aa5e0c1d448ef4145ed373bac34045f5dda4ecdb
hyeon-aa <ahhyun1120@naver.com>
2025-11-25 23:37:33 +0900

    fix: 마우스 올릴때 말고 클릭할때 prefetch로 변경

--- src/pages/Home/CategoryBox/CategoryBox.tsx
+++ src/pages/Home/CategoryBox/CategoryBox.tsx
@@ -29,6 +29,7 @@ export const CategoryBox = ({
     if (isTodayPublished) {
       openModal(<CompleteDailyQuestionModal />);
     } else {
+      prefetchQuestion(categoryId);
       openModal(<DailyQuestionModal categoryId={categoryId} key={categoryId} />);
     }
   };
@@ -37,7 +38,6 @@ export const CategoryBox = ({
     <>
       <div
         onClick={() => handleModalOpen(categoryId)}
-        onMouseEnter={() => prefetchQuestion(categoryId)}
         className={`rounded-xl p-4 relative bg-[var(--color-white)] ${size === 'large' ? 'flex-2 min-h-[136px]' : 'flex-1 min-h-[89px]'}
            transition-shadow duration-300 cursor-pointer
            hover:shadow-[0_0_30px_0_#D0D2D9] active:shadow-[0_0_30px_0_#D0D2D9]`}
