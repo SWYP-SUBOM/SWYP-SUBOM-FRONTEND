b5d1706467ea03d435336a8b396371b6b21a0897
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-11-26 14:56:32 +0900

    fix : 피드 내 글 반응달기 페이지로 수정

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -3,7 +3,6 @@ import { TitleHeader } from '../../components/common/TitleHeader';
 import { ReactionButtons } from '../../constants/ReactionButtons';
 import { useGetPost } from '../../hooks/Post/useGetPost';
 import { FeedLayout } from '../../layout/FeedLayout';
-import { CalendarPostView } from '../Calendar/CalendarPostView';
 import { PostDetailBox } from './_components/PostDetailBox';
 import { ReactionButton } from './_components/ReactionButton';
 
@@ -12,13 +11,7 @@ export const PostDetail = () => {
   const postId = Number(params.postId);
 
   const { data: postData } = useGetPost(postId);
-  const isMyPost = postData?.writer.me ?? false;
-
   const myReaction = postData?.myReaction?.reactionName;
-
-  if (isMyPost) {
-    return <CalendarPostView postId={postId} />;
-  }
 
   return (
     <>
