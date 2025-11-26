5001c9b17f2a006e7cc7e3b541c1b2c9a4edcb22
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-11-26 22:35:23 +0900

    fix : SSE 관련 코드 주석처리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useGetHome } from '../../hooks/Home/useGetHome';
-import { useNotificationStream } from '../../hooks/Notification/useNotificationStream';
+// import { useNotificationStream } from '../../hooks/Notification/useNotificationStream'; // SSE 관련 코드 주석처리
 import { useBottomSheet } from '../../hooks/useBottomSheet';
 import { useGetUserName } from '../../hooks/useGetUserName';
 import { useModal } from '../../hooks/useModal';
@@ -20,7 +20,7 @@ const Home = () => {
   const setTodayPostInfo = useTodayPostInfoStore((state) => state.setTodayPostInfo);
 
   // Home 페이지에 도착했을 때만 SSE 알림 스트림 연결
-  useNotificationStream();
+  // useNotificationStream(); // SSE 관련 코드 주석처리
 
   const isTodayDraft = homeData?.todayPost?.postStatus === 'DRAFT';
   const draftPostId = homeData?.todayPost?.postId;
