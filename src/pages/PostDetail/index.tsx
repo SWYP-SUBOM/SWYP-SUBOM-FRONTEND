0dc29f8243bf1a661e71a3cfdc3fa1f5aadbc806
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2025-11-28 22:03:02 +0900

    fix : 내가 쓴 글 반응하는 글로 수정

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -4,7 +4,6 @@ import { ReactionButtons } from '../../constants/ReactionButtons';
 import { useGetPost } from '../../hooks/Post/useGetPost';
 import type { ReactionNameType } from '../../hooks/Post/useToggleReaction';
 import { FeedLayout } from '../../layout/FeedLayout';
-import { CalendarPostView } from '../Calendar/CalendarPostView';
 import { PostDetailBox } from './_components/PostDetailBox';
 import { ReactionButton } from './_components/ReactionButton';
 
@@ -13,13 +12,8 @@ export const PostDetail = () => {
   const postId = Number(params.postId);
 
   const { data: postData } = useGetPost(postId);
-  const isMyPost = postData?.writer.me ?? false;
 
   const myReaction = postData?.myReaction?.reactionName;
-
-  if (isMyPost) {
-    return <CalendarPostView postId={postId} />;
-  }
 
   return (
     <>
