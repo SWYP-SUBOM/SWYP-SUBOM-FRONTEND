c570e2d56a4ae37a9f7d42e237d0c04e0b34e09c
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-04 05:43:08 +0900

    feat : 해당 월, 일, 요일 추가

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,18 @@
 import { Header } from '../../components/admin/common/Header';
 
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+const week = ['일', '월', '화', '수', '목', '금', '토'][new Date().getDay()];
+
 export const Admin = () => {
   return (
     <div>
       <Header title="써봄 워크스페이스" button="질문 삭제" />
+      <div className="w-full flex flex-col pl-4 pt-10 ">
+        <div className="B01_M text-gray-900">
+          {month}월 {day}일 ({week})
+        </div>
+      </div>
     </div>
   );
 };
