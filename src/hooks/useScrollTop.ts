37feac236d0621d6879203cba4f8f0c4ae162d77
hyeon-aa <ahhyun1120@naver.com>
2026-01-09 23:58:27 +0900

    feat : scroll top 기능 추가

---
+++ src/hooks/useScrollTop.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export const useScrollTop = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowButton(window.scrollY > 0);
+    };
+
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return { showButton, scrollToTop };
+};
