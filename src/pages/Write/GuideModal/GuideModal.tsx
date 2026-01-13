5711e3c1c1603911c427ad65e652e96c0af520cf
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-14 03:06:20 +0900

    feat : 글쓰기 이벤트 추가 완료

--- src/pages/Write/GuideModal/GuideModal.tsx
+++ src/pages/Write/GuideModal/GuideModal.tsx
@@ -1,10 +1,13 @@
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 import type { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 import xButton from '../../../assets/Modal/xbutton.svg';
 import { GUIDE_MAP, type guideTopicType } from '../../../constants/Guide';
 import { useModal } from '../../../hooks/useModal';
 import { GuideContent } from './GuideContent';
+
+import { GAEvents } from '../../../utils/GAEvent';
 
 interface GuideModalProps {
   topicType: guideTopicType;
@@ -52,6 +55,10 @@ export const Xbutton = () => {
 };
 
 export function GuideModal({ topicType }: GuideModalProps) {
+  useEffect(() => {
+    GAEvents.writingGuideClick();
+  }, []);
+
   return createPortal(
     <>
       <GuideOverlay>
