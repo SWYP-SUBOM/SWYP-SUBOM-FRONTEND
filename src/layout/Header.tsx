ff6415176fc1440980047a894d743f3f0b08e180
hyeon-aa <ahhyun1120@naver.com>
2026-01-29 20:29:43 +0900

    feat: 헤더 색상 변경

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <>
-      <div className="flex justify-between w-full px-4 pt-[14px]">
+      <div className="flex justify-between w-full px-4 pt-[14px] bg-[#F3F5F8]">
         <div className="flex gap-1">
           <img src={logoImg} className="w-9 h-6" />
           <img src={logoName} className="w-11 h-6" />
